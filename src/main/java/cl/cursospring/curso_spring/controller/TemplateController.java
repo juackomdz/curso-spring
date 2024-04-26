5daa4c82adc197f8ec31c8cdfe18b5005fcfaae1
Joaquin Mendez <joaquin.enmt@gmail.com>
2024-04-26 15:49:05 -0400

    Implementando thymeleaf

--- src/main/java/cl/cursospring/curso_spring/controller/TemplateController.java
+++ src/main/java/cl/cursospring/curso_spring/controller/TemplateController.java
@@ -1,8 +1,14 @@
 package cl.cursospring.curso_spring.controller;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 
 @Controller
@@ -10,8 +16,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TemplateController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+
+        String nombre = "joaquin";
+        String pais = "chile";
+        model.addAttribute("nombre", nombre);
+        model.addAttribute("country", pais);
         return "template/home";
     }
+
+    @GetMapping("/parametros")
+    public String getMethodName(Model model) {
+
+        Integer num1=11;
+        Integer num2=10;
+        Integer cifra=12345;
+        Date fecha = new Date();
+
+        List<String> paises =  new ArrayList<String>();
+        paises.add("chile");
+        paises.add("colombia");
+        paises.add("argentina");
+        paises.add("venezuela");
+
+        model.addAttribute("num1", num1);
+        model.addAttribute("num2", num2);
+        model.addAttribute("fecha", fecha);
+        model.addAttribute("cifra", cifra);
+        model.addAttribute("paises", paises);
+        return "template/parametros";
+    }
+    
     
 }
