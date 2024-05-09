6bf678b6d55132aa37a0301a088a7c624f320687
Joaquin Mendez <joaquin.enmt@gmail.com>
2024-05-09 15:01:05 -0400

    Commit formularios

---
+++ src/main/java/cl/cursospring/curso_spring/controller/formulario.java
@@ -0,0 +1,62 @@
+package cl.cursospring.curso_spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import cl.cursospring.curso_spring.model.UsuarioModel;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+
+@Controller
+@RequestMapping("/formulario")
+public class formulario {
+
+    //formulario de manera sencilla
+    //--------------------------------------------------
+    @GetMapping("/home")
+    public String home() {
+        return "formularios/home";
+    }
+    
+    @PostMapping("/home")
+    @ResponseBody
+    public String homePost(@RequestParam(name = "username")String username, @RequestParam(name = "password")String pass) {
+
+        return "hola "+username+" tu password es: "+pass;
+    }
+    //---------------------------------------------------
+
+    //formulario con objetos
+    //--------------------------------------------------
+    @GetMapping("/objeto")
+    public String objeto() {
+        return "formularios/objeto";
+    }
+    
+    @PostMapping("/objeto")
+    @ResponseBody
+    public String objetoPost(UsuarioModel modelo) {
+
+        return "<h1>Objeto</h1>hola "+modelo.getUsername()+" password: "+modelo.getPassword();
+    }
+
+    @GetMapping("/objeto2")
+    public String objeto2(Model model) {
+        model.addAttribute("usuario", new UsuarioModel());
+        return "formularios/objeto2";
+    }
+    
+    @PostMapping("/objeto2")
+    @ResponseBody
+    public String objeto2Post(UsuarioModel modelo) {
+        
+        return "<h1>Objeto 2</h1>hola "+modelo.getUsername()+" password: "+modelo.getPassword();
+    }
+    //---------------------------------------------------
+}
---
+++ src/main/java/cl/cursospring/curso_spring/model/UsuarioModel.java
@@ -0,0 +1,26 @@
+package cl.cursospring.curso_spring.model;
+
+public class UsuarioModel {
+
+    private String username;
+    private String password;
+
+    
+    public UsuarioModel() {
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    
+}
