0a4f9158bb60e50286490afce8aa96d11388b9d9
Joaquin Mendez <joaquin.enmt@gmail.com>
2024-08-06 17:21:51 -0400

    api rest 2

--- src/main/java/cl/cursospring/curso_spring/controller/apicontroller.java
+++ src/main/java/cl/cursospring/curso_spring/controller/apicontroller.java
@@ -1,15 +1,35 @@
 package cl.cursospring.curso_spring.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PutMapping;
+
+
 
 @RestController
 @RequestMapping("/api/v1")
 public class apicontroller {
 
     @GetMapping("/hola")
-    public String holamundo(){
-        return "Hola Mundo desde api";
+    public String holamundoget(){
+        return "Hola Mundo get desde api";
+    }
+
+    @PostMapping("/hola")
+    public String holaMundopost(){
+        return "Hola Mundo post desde api";  
+    }
+
+    @PutMapping("/hola")
+    public String holamundoput() {
+        return "hola mundo put desde api";
+    }
+
+    @DeleteMapping("/hola")
+    public String holaMundodelete(){
+        return "Hola Mundo delete desde api";       
     }
 }
